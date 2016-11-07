58e5d3b3f4a8a812f9b2f3608cae651e10f9eea2
fischeversenker <kontakt@felixhamann.de>
2016-11-07 20:18:04 +0100

    introduced animframe and degrees

--- app/src/js/drawingcanvas.js
+++ app/src/js/drawingcanvas.js
@@ -1,48 +1,105 @@
 (function($) {
+  "use strict";
 
   $(function() {
-    var canvas = $("#drawer").get(0);
-    var ctx = canvas.getContext("2d");
-    var running = false;
-    canvas.width  = $(window).width();
-    canvas.height = $(window).height();
 
-    var center = {
-      x: canvas.width / 2,
-      y: canvas.height / 2,
+    var bgCanvas = $("#drawer").get(0);
+    var bgCtx = bgCanvas.getContext("2d");
+    var drawingCanvas = $("<canvas/>").insertAfter($(bgCanvas)).get(0);
+    var drawingCtx = drawingCanvas.getContext("2d");
+
+    var running = false,
+        center;
+
+    var options = {
+      spineCount:       4,
+      spineColor:       "#ffffff",
+      backgroundColor:  "#000000",
+    };
+
+    bgCanvas.width  = $(window).width();
+    bgCanvas.height = $(window).height();
+    drawingCanvas.width  = $(window).width();
+    drawingCanvas.height = $(window).height();
+
+    $(window).resize(function() {
+      bgCanvas.width  = $(window).width();
+      bgCanvas.height = $(window).height();
+      drawingCanvas.width  = $(window).width();
+      drawingCanvas.height = $(window).height();
+      center.x = bgCanvas.width / 2;
+      center.y = bgCanvas.height / 2;
+    });
+
+    center = {
+      x: bgCanvas.width / 2,
+      y: bgCanvas.height / 2,
       radius: 4,
     };
-    var options = {
-      spines: 8,
-      spineColor: "#ffffff",
-    };
+
 
     running = true;
     draw();
 
 
     function draw() {
-      if(running) window.requestAnimationFrame(draw);
-      // clear canvas
-      ctx.fillStyle = "#000000";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      // if(running) window.requestAnimFrame(draw);
 
-      // draw center
+      // clear bgCanvas
+      bgCtx.fillStyle = options.backgroundColor;
+      bgCtx.fillRect(0, 0, bgCanvas.width, bgCanvas.height);
+
       drawSpines();
     }
 
     function drawSpines() {
-      for(var i = 0; i < options.spines; i++){
-        ctx.beginPath();
-        ctx.lineWidth = "1";
-        ctx.strokeStyle = options.spineColor;
-        ctx.moveTo(center.x, center.y);
+      var endPoint;
+      for(var i = 0; i < options.spineCount; i++){
+        bgCtx.beginPath();
+        bgCtx.lineWidth = "1";
+        bgCtx.strokeStyle = options.spineColor;
+        bgCtx.moveTo(center.x, center.y);
         // logic for spines missing
-        ctx.lineTo(250,75 * i);
-        ctx.stroke();
+        endPoint = getEndPoint(i);
+        bgCtx.lineTo(endPoint.x, endPoint.y);
+        bgCtx.stroke();
       }
+
+      function getEndPoint(i) {
+        var eP = {x: null, y: null};
+        var degOffset = (360 / options.spineCount) * i;
+        console.log(degOffset);
+
+
+        eP.x = center.x + i * 360 / options.spineCount;
+        eP.y = 0 + i;
+        return eP;
+      }
+
+
     }
-
   });
-
 })($);
+
+/** Converts numeric degrees to radians */
+if (typeof(Number.prototype.toRad) === "undefined") {
+  Number.prototype.toRad = function() {
+    return this * Math.PI / 180;
+  };
+}
+
+/** Converts radians to numeric (signed) degrees */
+if (typeof(Number.prototype.toDeg) === "undefined") {
+  Number.prototype.toDeg = function() {
+    return this * 180 / Math.PI;
+  };
+}
+
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
